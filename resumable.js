c78adba8f63e243a8193fecb616e9c3998a6fa2d
shanshuirenjia <978987373@qq.com>
2019-08-15 09:45:45 +0800

    optimized code

--- resumable.js
+++ resumable.js
@@ -501,7 +501,7 @@
       $.container = '';
       $.preprocessState = 0; // 0 = unprocessed, 1 = processing, 2 = finished
       $.error = null ; // error = null : success, error != null : error
-      $.lastTime = 0; // lastTime = 0 : uploaded, lastTime != 0 : uploading
+      $.remainingTime = 0; // remainingTime = 0 : uploaded, remainingTime != 0 : uploading
       $.isSaved = false;
       $.newFileName = $.fileName; // need a default name
       var _error = uniqueIdentifier !== undefined;
